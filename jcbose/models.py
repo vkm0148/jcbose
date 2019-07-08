75e8de7c86f566707310f7a614b9839ddb465656
vkm0148 <vinitandfriends@gmail.com>
2019-07-08 23:57:20 +0530

    profile creation

--- jcbose/models.py
+++ jcbose/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	
+def create_profile(sender, **kwargs):
+		if kwargs['created']:
+			user_profile = Profile.objects.create(user=kwargs['instance'])
 
-	def __str__(self):
-		return f'{self.user.username} Profile'
+
+post_save.connect(create_profile, sender=User)
