35e22b251c01216d19946c36e4e985d600bfe55d
raghavgupta4475 <raghavkumargupta18@gmail.com>
2019-07-09 01:28:46 +0530

    i dont know

--- jcbose/forms.py
+++ jcbose/forms.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django import forms
 
-class SignUpForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ('email',)
+class ProfileForm(forms.Form):
+	post = forms.CharField()
